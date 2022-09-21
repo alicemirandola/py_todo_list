9e78c19f097c1394fe670469d785a165665b5d67
alicemirandola <alice.mirandola@gmail.com>
2022-09-21 16:33:41 +0100

    Added versions with working db connectivity

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 # Use an official Python runtime as an image
-FROM python:3.6
+FROM python:3.9
+ENV fLASK_APP app
 
 # The EXPOSE instruction indicates the ports on which a container # # will listen for connections
 # Since Flask apps listen to port 5000  by default, we expose it
@@ -10,10 +11,21 @@ EXPOSE 5000
 # instruction creates a directory with this name if it doesn’t exist
 WORKDIR /app
 
+# install mariadb connector/c
+RUN wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
+RUN echo "733cf126b03f73050e242102592658913d10829a5bf056ab77e7f864b3f8de1f  mariadb_repo_setup"     | sha256sum -c -
+RUN chmod +x mariadb_repo_setup
+RUN ./mariadb_repo_setup    --mariadb-server-version="mariadb-10.6"
+RUN apt install libmariadb3 libmariadb-dev -y
+
 # Install any needed packages specified in requirements.txt
 COPY requirements.txt /app
 RUN pip install -r requirements.txt
 
+# Load secrects
+COPY .secrets_env_vars /app/.secrets_env_vars
+RUN . /app/.secrets_env_vars
+
 # Run app.py when the container launches
 COPY app.py /app
-CMD python app.py
+CMD ["flask", "run", "--host", "0.0.0.0"]
--- app.py
+++ app.py
@@ -1,7 +1,68 @@
 from flask import Flask, render_template, request
+import os
+import mariadb as database
+import sys
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    return '<h1>{}</h1>' .format(request.form['todoitem'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# Load env vars from current shell env
+# (source secret file first: source .secret_env_vars)
+username = os.environ.get("dbusername")
+password = os.environ.get("dbpassword")
+
+# Connect to MariaDB Platform
+try:
+    connection = database.connect(
+        user = username,
+        password = password,
+        host = "127.0.0.1",
+        port = 3306,
+        database = "todo_database"
+
+    )
+except database.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cursor = connection.cursor()
+
+# Adding data to database
+def add_data(todos_description, is_completed):
+    try:
+        statement = "INSERT INTO todos (todos_description,is_completed) VALUES (%s, %s)"
+        data = (todos_description, is_completed)
+        cursor.execute(statement, data)
+        connection.commit()
+        print("Successfully added entry to database")
+    except database.Error as e:
+        print(f"Error adding entry to database: {e}")
+
+# Retrieving data from database
+def get_data(is_completed):
+    try:
+      statement = "SELECT todos_description, is_completed FROM todos WHERE is_completed=%s"
+      data = (is_completed,)
+      cursor.execute(statement, data)
+      for (todos_description, is_completed) in cursor:
+        print(f"Successfully retrieved {todos_description}, {is_completed}")
+    except database.Error as e:
+      print(f"Error retrieving entry from database: {e}")
+
+# Verify that your code is running as expected
+add_data("test app", 1)
+get_data(1)
+
+connection.close()
